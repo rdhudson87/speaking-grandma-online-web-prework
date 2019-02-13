8fa8e1d948c3fc49cac8ef858270032710045128
Ricardo H <ricardo.hudson923@gmail.com>
2019-02-13 12:09:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,4 @@
 # Write a speak_to_grandma method.
-
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
@@ -11,3 +10,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+ "I love you grandma".upcase = "I Love you too pumpkin".upcase
+    if "I love you grandma".upcase = "Huh?! Speak up, Sonny".upcase
+      puts "Hi nana, how are you"
+      elsif ""
